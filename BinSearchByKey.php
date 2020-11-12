99838b7414361a38dfafeac5ba89a19e377180b4
Eugene Kostyukov <73199734+evgeniikostiukov@users.noreply.github.com>
2020-11-12 22:03:03 +0300

    Update BinSearchByKey.php

--- BinSearchByKey.php
+++ BinSearchByKey.php
@@ -1,17 +1,29 @@
 <?php
 
+
+function createFile($fileName, $count)
+{
+    $file = fopen($fileName, "w");
+    for ($i = 0; $i < $count; $i++) {
+        fwrite($file, "ключ" . $i . "\t" . "значение" . $i . "\x0A");
+    }
+}
+
+
 function runTime($time = false)
 {
-    return $time === false? microtime(true) : round(microtime(true) - $time, 3);}
+    return $time === false ? microtime(true) : round(microtime(true) - $time, 3);
+}
 
-function bin_searchByKey($file, $targetValue) {
+function bin_searchByKey($file, $targetValue)
+{
     $openfile = new SplFileObject($file);
     $left = 0;
     $right = count(file($file)) - 1;
     while ($left <= $right) {
         $mid = floor(($left + $right) / 2);
-        $openfile -> seek($mid);
-        $current_str = explode("\t", $openfile -> current());
+        $openfile->seek($mid);
+        $current_str = explode("\t", $openfile->current());
         $search = strnatcmp($current_str[0], $targetValue);
         if ($search > 0) {
             $right = $mid - 1;
@@ -24,10 +36,12 @@ function bin_searchByKey($file, $targetValue) {
     }
     return "undefined";
 }
-$file = './test.txt';
+
+//createFile("Keynumbers.txt", 2000000);
+$file = './Keynubmers.txt';
 $targetValue = 'ключ3';
-$time=runTime();
+$time = runTime();
 echo bin_searchByKey($file, $targetValue);
-$time=runTime($time);
-echo "<br>".$time." сек.<br>";
-echo round(memory_get_usage() / 1024, 2) ." Кб.";
+$time = runTime($time);
+echo "<br>" . $time . " сек.<br>";
+echo round(memory_get_usage() / 1024, 2) . " Кб.";
